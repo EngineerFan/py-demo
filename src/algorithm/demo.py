5eeddb73333c279a32d7891497e7559f5288a4f4
Ryan <faxilun@gmail.com>
2020-08-05 20:51:51 +0800

    feat: 4. 寻找两个正序数组的中位数 ing

--- src/algorithm/demo.py
+++ src/algorithm/demo.py
@@ -7,18 +7,20 @@
 
 s1 = set([-1, 0, 1])
 s2 = set([-1, 1, 0])
-print('s1:', s1)
-print('s2:', s2)
-print(s1 == s2)
-
-print(-1)
-d = dict()
-d['_1'] = 2
-print(d)
-print(str(1) == str)
-print('_' + str(1))
-print('-' * 100)
-l = [0, 0, 0]
-print(l.index(0))
-print(l.index(0))
-print(l.index(1,1,2))
+# print('s1:', s1)
+# print('s2:', s2)
+# print(s1 == s2)
+#
+# print(-1)
+# d = dict()
+# d['_1'] = 2
+# print(d)
+# print(str(1) == str)
+# print('_' + str(1))
+# print('-' * 100)
+# l = [0, 0, 0]
+# print(l.index(0))
+# print(l.index(0))
+# print(l.index(1,1,2))
+print(type(1.5) == int)
+print(round(1.5))
