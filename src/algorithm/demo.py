fb4e06f9846dacbe34fd0e2cdb3a33afd86d1d99
Ryan <faxilun@gmail.com>
2020-09-25 16:59:31 +0800

    feat:  589. N叉树的前序遍历

--- src/algorithm/demo.py
+++ src/algorithm/demo.py
@@ -5,9 +5,8 @@
 # @Description : demo
 
 
-def fib(n: int) -> int:
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
-    return fib(n - 1) + fib(n - 2)
+ll = [1, 2, 3]
+
+ll.insert(0, -1)
+ll.insert(1, -2)
+print(ll)
