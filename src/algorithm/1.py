e33807f3e2bcd27cb5f6009d742c4993980c3be5
FXL <fanxiaolu@yto.net.cn>
2021-03-03 18:45:37 +0800

    feat: 自动求导学习

--- src/algorithm/1.py
+++ src/algorithm/1.py
@@ -7,8 +7,13 @@ from typing import List
 
 
 class Solution:
-    def nextPermutation(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
+    def uniquePaths(self, m: int, n: int) -> int:
+        f = [[0] * n for _ in range(m)]
+        print(f)
 
+
+if __name__ == '__main__':
+    so = Solution()
+    m = 3
+    n = 4
+    print(so.uniquePaths(m, n))
---
+++ src/script/second_kill/__init__.py
@@ -0,0 +1,8 @@
+# author  : Ryan
+# datetime: 2021/3/1 10:49
+# software: PyCharm
+
+"""
+description:
+
+"""
