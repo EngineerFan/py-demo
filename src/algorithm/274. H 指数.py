ce48cec69d435f05479d078ac4cf20019da6dc66
Ryan <823896219@qq.com>
2021-07-11 20:11:03 +0800

    fix: 代码优化

--- src/algorithm/274. H 指数.py
+++ src/algorithm/274. H 指数.py
@@ -10,25 +10,14 @@ import collections
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         N = len(citations)
-        if N == 0:
-            return 0
-        if N == 1:
-            if citations[0] == 0:
-                return 0
-            else:
-                return 1
         citations.sort()
-        c = collections.Counter(citations)
         result = 0
-        if result == 0 and N == c[result]:
-            return result
         for i in range(N):
-            if (i == 0 and citations[i] >= N - i) or (citations[i] >= N - i and citations[i - 1] < citations[i]) or (
-                    citations[i] >= N - i and citations[i - 1] == citations[i]):
+            if citations[i] >= N - i and (i == 0 or citations[i - 1] <= citations[i]):
                 result = max(N - i, result)
         return result
 
 
 so = Solution()
-citations = [2, 2, 2]
+citations = [0]
 print(so.hIndex(citations))
