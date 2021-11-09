9cfdce1e5b4fad2d6f36646d8ffff2a17b8d572e
FXL <fanxiaolu@yto.net.cn>
2021-11-09 21:53:29 +0800

    Revert "feat: 快速排序"
    This reverts commit 2964eb16

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -6,24 +6,6 @@
 description:
 
 """
-from typing import List
 
-
-def sortColors(nums: List[int]) -> List[int]:
-    zero, i = 0, 0
-    two = len(nums)
-    while i < two:
-        if nums[i] == 0:
-            nums[i], nums[zero] = nums[zero], nums[i]
-            i += 1
-            zero += 1
-        elif nums[i] == 1:
-            i += 1
-        else:
-            two -= 1
-            nums[i], nums[two] = nums[two], nums[i]
-    return nums
-
-
-nums = [2, 0, 2, 1, 1, 0]
-print(sortColors(nums))
+l = [2, 45, 6, 1, 35]
+print([i for i in l if i > 1])
